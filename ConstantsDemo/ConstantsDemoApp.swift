ecebff9ca1a964cabc3336f2befa6f3246443699
Davide Troise <davide@troise.net>
2022-10-24 22:01:14 +0200

    Initial Commit

---
+++ ConstantsDemo/ConstantsDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  ConstantsDemoApp.swift
+//  ConstantsDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct ConstantsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
